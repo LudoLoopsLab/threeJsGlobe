4b10f35cc655eba87f6f2a76f05185abe5259401
Ludovic Dumas <ludoloops@pm.me>
2023-09-18 23:16:08 -0400

    alter with shader

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 import * as THREE from 'three'
+import vertexShader from './shaders/vertex.glsl'
+import fragmentShader from './shaders/fragment.glsl'
+
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
@@ -10,14 +13,23 @@ document.body.appendChild(renderer.domElement)
 
 
 // crate a sphere
-const sphere = new THREE.Mesh(new THREE.SphereGeometry(5, 50, 50), new THREE.MeshBasicMaterial({
-  // color: 0xff0000
-  map: new THREE.TextureLoader().load('./img/earth.jpg')
-}
-))
+const sphere = new THREE.Mesh
+  (new THREE.SphereGeometry(5, 50, 50),
+    new THREE.ShaderMaterial({
+      vertexShader,
+      fragmentShader,
+      uniforms: {
+        globeTexture: {
+          // value: new THREE.TextureLoader().load('./img/earth.jpg')
+          value: new THREE.TextureLoader().load('./img/deathStar.png')
+        }
+      }
+    })
+  )
+
 
 scene.add(sphere)
-camera.position.z = 10
+camera.position.z = 15
 
 function animate() {
   requestAnimationFrame(animate)
